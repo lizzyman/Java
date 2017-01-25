499d0b987b2549897be89767bd6bcd7d99ff5fa5
lizzyman <lizzysoo0000@gmail.com>
2017-01-25 10:08:58 +0900

    2017-01-24 [객체 지향 프로그래밍]

--- VendingMachine/src/com/ktds/lizzy/Transaction.java
+++ VendingMachine/src/com/ktds/lizzy/Transaction.java
@@ -1,11 +1,14 @@
 package com.ktds.lizzy;
 
+import java.util.Scanner;
 import com.ktds.lizzy.person.*;
 import com.ktds.lizzy.machine.VendingMachine;
 
 public class Transaction {
 	
 	public void start() {
+		Scanner input = new Scanner(System.in);
+		 
 		System.out.println("<User's info>");
 		User lizzy = new User(2000);
 		
@@ -13,10 +16,36 @@ public class Transaction {
 		int[] productQuantity = {10, 5, 3};
 		VendingMachine vendingMachine = new VendingMachine(10000, productQuantity);
 		
-		System.out.println("\n=====Transaction Start!=====");
-		lizzy.buy(vendingMachine, "Soda", 700);
-		System.out.println("=====Transaction Finish!=====\n");
+		int inputMoney = 0; 
+		String productName;
+		String stopSign;
 		
+		while(true) {
+			
+			System.out.println("\n=====Transaction Start!=====");
+			
+			System.out.println("Put your Money to the Vending Machine.");
+			inputMoney = input.nextInt();
+			
+			System.out.println("What do you want to drink? [Coke, Soda, MilkTea]");
+			productName = input.next();
+			
+			lizzy.buy(vendingMachine, productName, inputMoney);
+			
+			System.out.println("Do you want to Continue? (y/n)");
+			stopSign = input.next();
+			
+			if (stopSign.equals("n")) {
+				System.out.println("=====Transaction Finish!=====\n");
+				break;		
+			}
+			else if (stopSign.equals("y"))
+				System.out.println("=====Transaction Continue!=====\n");
+			else 
+				System.out.println("You answered WRONG!!! Try Again.");
+			
+		}
+
 		System.out.println("<User's info>");
 		System.out.println(lizzy);
 		
